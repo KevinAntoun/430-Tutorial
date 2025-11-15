952dcedc151ba9f2709667d7b48b31293306b6c9
Kevin Antoun <kevinantoun007@gmail.com>
2025-11-15 15:14:53 +0200

    HI

--- ps2/ps2/settings.py
+++ ps2/ps2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9t^jthkkm8pzfymb2b&pq64l(yc-1ug-bxvp+)t+_x10(xr&0@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'ps2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tasks',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres-service',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
